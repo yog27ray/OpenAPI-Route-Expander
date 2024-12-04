0ff2f63773f0752f7e6c5c535fb385b08efcbc2d
yog27ray <yog27ray@gmail.com>
2024-12-04 17:46:24 +0530

    feat: Added support for nested schema.

--- test/openapi-route-expander.test.ts
+++ test/openapi-route-expander.test.ts
@@ -2,19 +2,19 @@ import { resolveRefsInRoutes } from '../';
 import { FileUtil } from '../src/util/file.util';
 
 describe('resolveRefsInRoutes', () => {
-  test('throw error when input path is not absolute', () => {
+  it('throw error when input path is not absolute', () => {
     expect(() => resolveRefsInRoutes('../example/input/openapi.yaml', '../out/openapi.yaml'))
       .toThrow(Error('Path must be absolutePath. "../example/input/openapi.yaml"'));
   });
-  test('throw error when output path is not absolute', () => {
+  it('throw error when output path is not absolute', () => {
     expect(() => resolveRefsInRoutes(`${__dirname}/../example/input/openapi.yaml`, '../out/openapi.yaml'))
       .toThrow(Error('Path must be absolutePath. "../out/openapi.yaml"'));
   });
-  test('throw error when input path does not exist', () => {
+  it('throw error when input path does not exist', () => {
     expect(() => resolveRefsInRoutes(`${__dirname}/../example/input/openapi-1.yaml`, `${__dirname}/../out/openapi.yaml`))
       .toThrow(Error(`Path doesn't exists. "${__dirname}/../example/input/openapi-1.yaml"`));
   });
-  test('writes the output of the file.', () => {
+  it('writes the output of the file.', () => {
     resolveRefsInRoutes(`${__dirname}/../example/input/openapi.yaml`, `${__dirname}/../out/openapi.yaml`);
     const originalContent = FileUtil.readFile(`${__dirname}/../example/output/openapi.yaml`);
     const generatedContent = FileUtil.readFile(`${__dirname}/../out/openapi.yaml`);
